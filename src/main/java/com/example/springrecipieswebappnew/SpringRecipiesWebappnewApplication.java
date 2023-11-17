20ec9626ce7a0346abcd20f4481b35d3456ac6d4
Vladimir <lil.rhymes9@gmail.com>
2023-11-18 02:59:09 +0300

    Migrations added

---
+++ src/main/java/com/example/springrecipieswebappnew/SpringRecipiesWebappnewApplication.java
@@ -0,0 +1,13 @@
+package com.example.springrecipieswebappnew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRecipiesWebappnewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRecipiesWebappnewApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springrecipieswebappnew/SpringRecipiesWebappnewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springrecipieswebappnew;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRecipiesWebappnewApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
