a576fbd4cdb5e4560e9f6bb7c72791e75d059b84
Vladimir <lil.rhymes9@gmail.com>
2023-11-18 18:35:34 +0300

    Template home

--- src/main/java/com/example/springrecipieswebappnew/Controller/UserController.java
+++ src/main/java/com/example/springrecipieswebappnew/Controller/UserController.java
@@ -1,8 +1,14 @@
 package com.example.springrecipieswebappnew.Controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class UserController {
 
+    @GetMapping(path = "/")
+    public String home(){
+
+        return "home";
+    }
 }
